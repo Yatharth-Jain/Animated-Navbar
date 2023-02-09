ba90ea048d2b9e180039be122eb5130fb956f677
Yatharth Jain <yatharthjain41@gmail.com>
2023-02-09 22:31:14 +0530

    Fixes

--- pages/pricing.js
+++ pages/pricing.js
@@ -1,14 +1,8 @@
 import Navbar from "../src/components/Navbar/Navbar";
 import { AnimatePresence , motion } from 'framer-motion'
 import { useRouter } from 'next/router'
-import { useState } from "react";
 
-export default function pricing(props) {
-  var route=useRouter()
-  var [show,setShow]=useState(1);
-  setTimeout(()=>{
-    setShow(0);
-  },4000)
+export default function pricing() {
   return (
     <>
       {/* <motion.div initial={{y:'100vh'}} animate={{y:0}} exit={{y:'-200px' ,position:'absolute'}} > */}
@@ -19,12 +13,4 @@ export default function pricing(props) {
       {/* </motion.div> */}
     </>
   )
-}
-
-export async function getServerSideProps(){
-  return{
-    props:{
-      data:"Hi"
-    }
-  }
 }
