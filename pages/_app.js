f1c864d2a67e9f016c57c92c15e6f59b54ef1eb9
Yatharth Jain <yatharthjain41@gmail.com>
2023-02-10 05:01:53 +0530

    Added mobile responsivenss

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,30 @@
-import '../styles/globals.css'
-import { AnimatePresence , motion } from 'framer-motion'
+import "../styles/globals.css";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  var route=useRouter()
+  var route = useRouter();
   return (
     <>
       <AnimatePresence>
-        <motion.div  key={route.pathname} initial={{y:'100vh'}} animate={{y:['1200px','900px','600px','300px','0px']}} exit={{y:['0px','0px','0px','0px','-300px'],position:'absolute',width:'100%'}} transition={{duration:0.75,ease:'linear'}} > 
-          <Component {...pageProps}/>
-        </motion.div> 
+        <motion.div
+          key={route.pathname}
+          initial={{ y: "100vh" }}
+          animate={{ y: ["200%","0%"] }}
+          exit={{
+            y: ["0%", "0%", "0%", "-60%"],
+            position: "absolute",
+            width: "100%",
+            cursor: "default",
+          }}
+          transition={{ duration: 1, ease: [0.2, 0.56, .76, 1] }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
       </AnimatePresence>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,44 +1,43 @@
 import Link from "next/link";
+import { useRef, useState } from "react";
+// import hamburger from '../../../public/image/hamburger-logo.png'
 
 export default function Navbar() {
+  var navbardiv = useRef(null);
+  const [shownav,setShownav]=useState(0);
   return (
     <nav id="navbar">
-      <ul>
+      <Link href="/" className="logo">
+        LOGO
+      </Link>
+      <ul className={`links ${shownav==1 && 'shownavdiv'}`} ref={navbardiv}>
         <li>
-          <Link
-            href="/"
-            style={{
-              fontSize: 30,
-              fontWeight: 900,
-              marginLeft: 20,
-              color: "black",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Link>
-        </li>
-        <li>
-          <Link href="/" className="navlinks">
+          <Link href="/" className="navlinks" >
             Home
           </Link>
         </li>
         <li>
-          <Link href="/about" className="navlinks">
+          <Link href="/about" className="navlinks" >
             About
           </Link>
         </li>
         <li>
-          <Link href="/pricing" className="navlinks">
+          <Link href="/pricing" className="navlinks" >
             Pricing
           </Link>
         </li>
         <li>
-          <Link href="/contact" className="navlinks">
+          <Link href="/contact" className="navlinks" >
             Contact
           </Link>
         </li>
       </ul>
+      <img
+        src="/image/hamburger-logo.png"
+        className="hamburgerbtn"
+        alt=""
+        onClick={() => setShownav(shownav^1)}
+      />
     </nav>
   );
 }
